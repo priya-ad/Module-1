852352d468ca5c6dd6147a1b0b3f14db291cd8f2
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-28 16:06:59 +0530

    Update Program-1.c

--- Program-1.c
+++ Program-1.c
@@ -1,3 +1,7 @@
+Make the following changes in the code given below:
+1. Comment the statement which prints "Mango".
+2. Remove the comment on the statement which prints "Banana"
+
 #include <stdio.h>
 void main() {
 	printf("Orange\n");
