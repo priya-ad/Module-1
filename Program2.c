2bb2904c99b4692e62c97f43ee64e170d754802f
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 14:21:20 +0530

    Create Program2.c

---
+++ Program2.c
@@ -0,0 +1,15 @@
+//The code given below contains text that prints "DennisRitchieBrianKernighan".
+
+//Make the suggested changes to the code so that it prints "DennisRitchieBrianKernighan" as shown below.
+//Dennis	Ritchie
+//Brian	Kernighan
+
+#include <stdio.h>
+void main() {
+	printf("DennisRitchieBrianKernighan\n");
+}
+Ans:
+#include <stdio.h>
+void main() {
+	printf("Dennis\t Ritchie\nBrian\tKernighan\n");
+}
