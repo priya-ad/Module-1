c2313a1a069fbd9a9a5250578d9a4a7873385ff6
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 15:04:26 +0530

    Update Program-1.c

--- Program-1.c
+++ Program-1.c
@@ -1,7 +1,7 @@
 //Make the following changes in the code given below:
 //1. Comment the statement which prints "Mango".
 //2. Remove the comment on the statement which prints "Banana"
-
+//Read the ReadME file for more information
 
 #include <stdio.h>
 void main() {
