b563182a623d57b378b0a2edea1793b2f841a97d
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 14:35:39 +0530

    Create Program-4.c

---
+++ Program-4.c
@@ -0,0 +1,6 @@
+//Read the code given below to familiarize yourself with the syntax of main() function. Retype in the space provided.
+#include <stdio.h>
+int MAIN() {
+	printf("Hello, float data type allocates 4 bytes in memory");
+	return 0;
+}
