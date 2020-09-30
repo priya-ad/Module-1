21fbcc81bed68f21d93fb3599111f1854d0e9928
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 15:10:30 +0530

    Create Program-7.c

---
+++ Program-7.c
@@ -0,0 +1,18 @@
+//perform typeconversion operation
+#include<stdio.h>
+int main()
+{
+    int a = 25, b = 13;
+    float result;
+
+    result = a/b;
+
+    // display only 2 digits after decimal point
+    
+
+   // write the typecasting code 
+
+    // display only 2 digits after decimal point
+        
+    return 0;
+}
