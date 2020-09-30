e81748f25a46150fd3add0ddd5d15f8aad8ce48b
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 15:09:18 +0530

    Create Program-6.c

---
+++ Program-6.c
@@ -0,0 +1,9 @@
+//Identify the errors in the code and correct them
+#include <stdio.h>
+void main() {
+	floating num1 = 5.34, num2 = 125.789f, result;
+	printf("Given float values are num1 = , num2 = \n", num1, num2);
+	result = num2 / num1;
+	printf("The result after dividing in float format = \n", );
+	printf("The result after dividing in exponential format = %e\n", );
+}
