62e3ca5ae9b2863118bc269ef40bf29f83301926
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-09-30 14:34:35 +0530

    Create Program-3.c

---
+++ Program-3.c
@@ -0,0 +1,8 @@
+//Correct the identifiers in the following program
+#include <stdio.h>
+int main()
+{
+  int first value=10, 2_value =20, third-value=30;
+printf(“x=%d,y=%d, z=%d”, first value, 2_value, third-value);
+  return 0;
+}
