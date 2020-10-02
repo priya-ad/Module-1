dd04621d7c12070602c6914f3cd7a608010d9ec3
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-10-02 10:23:32 +0530

    Create Program-8.c

---
+++ Program-8.c
@@ -0,0 +1,11 @@
+Write a program to read two integers from standard input and to print the sum of the two given integers on the standard output.
+
+During execution, the program should print the following message on the console:
+Enter two integer values : 
+For example, if the user gives the input as:
+Enter two integer values : 3 4
+then the program should print the result as:
+The sum of given numbers = 7
+Note - 1 : Use '\n' at the end while printing the final output using printf().
+
+Note - 2 : getchar() is used to read only one character at a time, where as scanf() can read multiple data type values at a time.
