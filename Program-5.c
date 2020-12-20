9d3d1a62eeafaef2406be52953ab174ff54308f5
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-12-20 18:43:12 +0530

    Update Program-5.c

--- Program-5.c
+++ Program-5.c
@@ -1,8 +1,15 @@
-//Complete the below given code by declaring the integer variable, adding their values and printing the sum.
-#include <stdio.h>
-void main() {
-	//variable declaration;
-	printf("Given integers are num1 = , num2 = \n", num1, num2);
-	//Write the code to add num1 and num2 and place the result in the variable sum
-	printf("Sum of 2 given numbers = %d\n", sum);
+//INPUT: 5 7
+//OUTPUT: 7 13    6 14
+#include<stdio.h>
+ int main ()
+{
+     int i, j;
+printf(“Enter the values of i and j”)
+scanf(“%d%d”, &i,&j);
+    
+ i = i + 1;
+      j = j + i + 1;
+      printf("%d %d \t", i + 1, j - 1);
+      printf("%d %d", i, j);
+return 0;
 }
