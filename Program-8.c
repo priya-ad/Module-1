fe0955bbe73bc18adbca7c113d4b5f608042b6a7
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-12-20 22:14:21 +0530

    Update Program-8.c

--- Program-8.c
+++ Program-8.c
@@ -3,23 +3,16 @@
 
 int main()
 {
-    int x = 12, y = 1;
+    int x = 12, y;
     printf(" Enter the values of x and y");
     sacnf("%d%d", &x,&y);
-    
-   // y = ++x; // increment the value of x by 1 then assign this new value to y
-
-   // printf("After incrementing by 1: x = %d\n", x);
+       
     printf("y = %d  ", y=++x);
     printf("y= %d  ",y=x++);
-  //  y = --x; // decrement the value of x by 1 then assign this new value to y
-
-    //printf("After decrementing by 1: x = %d\n", x);
-    printf("y = %d  ", y=--x);
-  printf("y=%d  ",y=x--);
   
-
-    // Signal to operating system everything works fine
-    return 0;
+   printf("y = %d  ", y=--x);
+   printf("y=%d  ",y=x--);
+  
+     return 0;
 }
 
