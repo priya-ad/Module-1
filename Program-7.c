9eb379f78011acc12f1b68acd206b056d363ceb5
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-12-20 19:33:08 +0530

    Update Program-7.c

--- Program-7.c
+++ Program-7.c
@@ -1,4 +1,5 @@
-
+//INPUT: 95 90.99 'A'
+//OUTPUT: i value is 90 x value is 90.00000 i value is 65
 
 #include<stdio.h>
 int main(){
@@ -8,10 +9,10 @@ int main(){
    printf(" Enter an integer value, floating point value and a character");
    scanf("%d%f%c", &i,&x,&ch);
    i = x ;
-   printf("i value is %d\n",i);
+   printf("i value is: %d ",i);
    x = i ;
-   printf("x value is %f\n",x);
+   printf("x value is: %f ",x);
    i = ch ;
-   printf("i value is %d\n",i);   
+   printf("i value is: %d ",i);   
 return 0;
 }
