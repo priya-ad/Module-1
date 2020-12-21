071f8ea8fa60929c5163b5b421e18ed4d0d12955
Radhika2611 <radhikaanugraha@gmail.com>
2020-12-21 05:50:38 +0000

    aa

--- Program-3.c
+++ Program-3.c
@@ -2,13 +2,12 @@
 //OUTPUT: Product= 2.69
 #include <stdio.h>
 int main() {
-    double a, b, product;
+    double a,b,product;
     //printf("Enter two numbers: ");
     scanf("%lf%lf",&a,&b);  
      // Calculating product
     product = a * b;
     // Result up to 2 decimal point is displayed using %.2lf
     printf("Product = %.2lf", product);
-    
-    return 0;
+return 0;
 }
