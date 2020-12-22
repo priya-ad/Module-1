d6bb30b7526b648c08343f6e3cd9c2a95c7ef12f
Radhika2611 <radhikaanugraha@gmail.com>
2020-12-22 04:39:50 +0000

    aa

--- Program-10.c
+++ Program-10.c
@@ -7,11 +7,8 @@ int main()
    float num, root;
    printf("Enter a number: ");
    scanf("%f", &num);
-
    // Computes the square root of num and stores in root.
    root = sqrt(num);
-
-
    printf("Square root of %.2f = %.2f", num, root);
    return 0;
 }
