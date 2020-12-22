fb5175d9264c8d7206367cf84a47c938f678783f
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-12-22 09:19:52 +0530

    Update Program-8.c

--- Program-8.c
+++ Program-8.c
@@ -1,18 +1,15 @@
 //INPUT:12
 //OUTPUT: y=13  y=13  y=13  y=13
 #include<stdio.h>
-
 int main()
 {
     int x, y;
     printf(" Enter the values of x: ");
     scanf("%d", &x);
-     printf("y = %d  ", y=++x);
+    printf("y = %d  ", y=++x);
     printf("y= %d  ",y=x++);
-  
-   printf("y = %d  ", y=--x);
-   printf("y=%d  ",y=x--);
-  
+    printf("y = %d  ", y=--x);
+    printf("y=%d  ",y=x--);
     return 0;
 }
 
