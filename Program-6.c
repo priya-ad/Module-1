b93f92e6e21b12c4fcd8ba603c13428893da8888
Radhika2611 <66779245+Radhika2611@users.noreply.github.com>
2020-12-31 20:04:37 +0530

    Update Program-6.c

--- Program-6.c
+++ Program-6.c
@@ -1,12 +1,13 @@
 //INPUT: 10 2
 //OUTPUT: Arithmetic expression evaluation of 10 and 2 is = 20
+
 #include <stdio.h>
 int main()
 {
 int a,b,result;
 printf("Enter 2 numbers for Arithmetic evaluation operation:");
-scanf("%d%d",&a,&b);
-result = a+b*a/b-a%b;
-printf("Arithmetic expression evaluation of %d and %d is = %d",a,b,result);
-return 0;
+scanf("%d%d",);  // correct the code
+r = a+b*a/b-a%b;      // correct the code
+printf("Arithmetic expression evaluation of %d and  is = %d",a,b,result); // correct the code
+// insert the missing code
 }
